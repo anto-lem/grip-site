8b11711e8d357d6be2fd2ea9f5c28d0b178cb12a
anto-lem <antoine.lemieux10@gmail.com>
2026-04-23 23:10:31 -0400

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Grip : Your Marketing System',
+  description: 'Stop paying agencies. Build your own marketing system. Get certified.',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
